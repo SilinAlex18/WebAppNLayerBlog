294cd19d4f5f7c16724cc57e17e33f83755debec
AlexSilin <AlexSilin@DESKTOP-J13R395>
2020-07-17 13:33:40 +0300

    WebAppNLayerBlog Areas

---
+++ WebAppNLayerBlog/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace WebAppNLayerBlog.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- WebAppNLayerBlog/Startup.cs
+++ WebAppNLayerBlog/Startup.cs
@@ -48,6 +48,11 @@ namespace WebAppNLayerBlog
 
             app.UseEndpoints(endpoints =>
             {
+                endpoints.MapAreaControllerRoute(
+                    name: "areas",
+                    areaName: "Admin",
+                    pattern: "Admin/{controller=Home}/{action=Index}/{id?}");
+
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
